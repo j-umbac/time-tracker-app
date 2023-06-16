e58741bfacbfd2d0778b279f883fe293aad540c3
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-16 15:00:25 +0800

    feat: add form components

--- src/plugins/primevue.ts
+++ src/plugins/primevue.ts
@@ -1,6 +1,12 @@
 import PrimeVue from 'primevue/config'
+import Card from 'primevue/card'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
 
 //Autoimport Primevue components to project
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(PrimeVue, { ripple: true });
+    nuxtApp.vueApp.use(PrimeVue, { ripple: true })
+    nuxtApp.vueApp.component('Card', Card)
+    nuxtApp.vueApp.component('Button', Button);
+    nuxtApp.vueApp.component('InputText', InputText);
 });
