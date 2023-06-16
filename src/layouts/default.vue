95b4aab639cc92d9611fa512bd2f2b9cf1cbdb7b
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-16 16:09:15 +0800

    feat: add default layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="min-h-[100dvh] h-full bg-surface-ground">
+        <div class="border-b bg-surface-card">
+            <Topbar class="container mx-auto" />
+        </div>
+        <main>
+            <div class="container mx-auto grid grid-cols-12">
+                <div class="col-span-12">
+                    <slot />
+                </div>
+            </div>
+        </main>
+    </div>
+    <div>
+        <PageFooter></PageFooter>
+    </div>
+</template>
