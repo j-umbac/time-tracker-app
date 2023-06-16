d134d950dd7b49e901525728ffa75b6290d9bc81
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-16 14:38:02 +0800

    infra: tailwind css config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,17 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   srcDir: './src',
+  modules: [
+    [
+      '@nuxtjs/tailwindcss', {
+        cssPath: '~/assets/styles/tailwind.css'
+      }
+    ]
+  ],
+  css: [
+    'primeicons/primeicons.css',
+    'primevue/resources/primevue.css',
+    'primevue/resources/themes/lara-light-blue/theme.css',
+    '@/assets/styles/main.css', // NOTE: ORDER IS IMPORTANT HERE, WE MUST BE ABLE TO OVERRIDE PRIMEVUE STYLES
+  ]
 })
