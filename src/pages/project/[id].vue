926d995e706b9b3ccc2c169effb0fe15e0a648c8
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-16 17:07:31 +0800

    feat: add project page

---
+++ src/pages/project/[id].vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="space-y-8 my-8">
+        <Card class="mx-8">
+        <template #title>
+            <div>
+
+                <span>Project Instance</span> 
+            </div>
+        </template>
+    </Card>
+    </div>
+    
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
