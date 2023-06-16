0ab5e0446b24e65db289877c24444dfe8177728a
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-16 15:25:11 +0800

    fix: login page

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,10 +1,15 @@
 <template>
-    <div class="flex h-screen sm:flex-row flex-col items-center justify-center container mx-auto">
-        <h1 class="text-h1 font-bold text-on-surface">Keepr</h1>
-        <p>The straitforward tracker for teams</p>
-        <LoginCard></LoginCard>
+    <div class="flex h-screen sm:flex-row flex-col sm:justify-between justify-center container mx-auto space-y-8">
+        <div class="mx-8">
+            <h1 class="text-h1 font-bold text-on-surface">Keepr</h1>
+            <p class="text-h4 text-on-surface">The straightforward project tracker for teams.</p>
+        </div>
+        <LoginCard class="h-96 mx-8"></LoginCard>
     </div>
 </template>
 
 <script setup>
+definePageMeta({
+    layout: 'no-layout'
+})
 </script>
