513844ae47dfa639de3d712db430cc4e4f4faca9
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-16 15:20:23 +0800

    feat: add login details

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        Login Page
+    <div class="flex h-screen sm:flex-row flex-col items-center justify-center container mx-auto">
+        <h1 class="text-h1 font-bold text-on-surface">Keepr</h1>
+        <p>The straitforward tracker for teams</p>
+        <LoginCard></LoginCard>
     </div>
 </template>
 
