22c49d4dc83d3bb2918aa683c400dae3d8162278
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-16 17:06:56 +0800

    feat: add project card component

---
+++ src/components/project-card.vue
@@ -0,0 +1,17 @@
+<template>
+    <Card class="bg-gradient-to-tr from-blue-500 to-cyan-300" @click="navigateTo(`/project/${project.id}`)">
+        <template #title>
+            <div class="text-h5 text-on-primary">
+                {{ project.name }}
+            </div>
+        </template>
+    </Card>
+</template>
+
+<script setup>
+const {props: project }= defineProps(['project'])
+</script>
+
+<style lang="scss" scoped>
+
+</style>
