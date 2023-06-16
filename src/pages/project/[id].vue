b6198e04e4ecf449f40f15f91d23f92bc8b97b47
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-16 18:07:03 +0800

    fix: proper fetching

--- src/pages/project/[id].vue
+++ src/pages/project/[id].vue
@@ -1,30 +1,28 @@
 <template>
     <div class="space-y-8 my-8">
         <Card class="mx-8">
-        <template #title>
-            <div class="flex flex-col space-y-2 items-start">
-                <Button icon="pi pi-arrow-left" raised rounded text @click="navigateTo({name: 'index'})" />
-                <span class="text-h">{{ project.name }}</span> 
-            </div>
-        </template>
-        <template #content>
-            <div>
-                <p>{{ project.description }}</p>
-            </div>
-        </template>
-    </Card>
+            <template #title>
+                <div class="flex flex-col space-y-2 items-start">
+                    <Button icon="pi pi-arrow-left" raised rounded text @click="navigateTo({ name: 'index' })" />
+                    <span class="text-h">{{ project.name }}</span>
+                </div>
+            </template>
+            <template #content>
+                <div>
+                    <p>{{ project.description }}</p>
+                </div>
+            </template>
+        </Card>
     </div>
-    
 </template>
 
 <script setup>
-const { id } = useRoute().params
+const id  = useRoute().params
+
 const { retrieveProject, project } = useProject()
 
-retrieveProject(id)
+await retrieveProject(id.id)
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
