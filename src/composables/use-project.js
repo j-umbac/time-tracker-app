805c67ed4e870c760f57d7dec13d5d273aa2b5fd
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-16 18:05:03 +0800

    feat: fetch single project

---
+++ src/composables/use-project.js
@@ -0,0 +1,27 @@
+export function useProject() {
+    const supabase = useSupabaseClient()
+    const errorMsg = ref()
+    const project = ref()
+
+    async function retrieveProject(id) {
+        const { data, error } = await supabase
+            .from("projects")
+            .select("*")
+            .eq("id", id)
+            .single();
+        if (error) {
+            console.log(error)
+            errorMsg.value = error
+        } else {
+            project.value = data
+            console.log("Running", project.value)
+            errorMsg.value = null
+        }
+    }
+
+    return {
+        errorMsg,
+        retrieveProject,
+        project
+    }
+}
