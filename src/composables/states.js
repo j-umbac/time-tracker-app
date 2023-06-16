5f01b6161534b528e617fbb27fc696b03d316d88
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-16 16:03:25 +0800

    feat: add global states

---
+++ src/composables/states.js
@@ -0,0 +1,6 @@
+// Global state for logged in user
+export const useCurrentUser = () => useState('user', () => ({}))
+// Global state for user's projects
+export const useUserProjects = () => useState('userProjectList', () => ([]))
+//Global state for projects
+export const useProjects = () => useState('projectList', () => ([]))
