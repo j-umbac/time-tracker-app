978e71189e2a38c79d2fc97780924819493d57ca
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-16 17:05:45 +0800

    feat: add project getter composable

---
+++ src/composables/use-current-user-projects.js
@@ -0,0 +1,26 @@
+export function useCurrentUserProjects() {
+    const supabase = useSupabaseClient()
+    const userProjectList = useUserProjects()
+    const user = useCurrentUser()
+    const errorMsg = ref()
+
+    async function retrieveUserProjects() {
+        const { data, error } = await supabase
+            .from("users_projects")
+            .select("*, projects(*)")
+            .eq('user_id', 2)
+        if (error) {
+            console.log(error)
+            errorMsg.value = error
+        } else {
+            userProjectList.value = data
+            errorMsg.value = null
+            console.log(userProjectList.value)
+        }
+    }
+
+    return {
+        errorMsg,
+        retrieveUserProjects
+    }
+}
