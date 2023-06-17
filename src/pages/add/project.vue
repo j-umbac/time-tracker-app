f7c2ac7bd897c1f579a1e9e46fd06cf8338ac2d0
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-17 19:24:03 +0800

    feat: add new project page

---
+++ src/pages/add/project.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="space-y-8 my-8">
+        <Card class="mx-8">
+            <template #title>
+                <div class="flex space-x-2 items-center">
+                    <Button icon="pi pi-arrow-left" raised rounded text @click="navigateTo({name: 'index'})" />
+                    <span class="text-h">New Project</span>
+                </div>
+            </template>
+        </Card>
+    </div>
+</template>
+
+<script setup>
+const { addProject } = useCurrentUserProjects()
+const err = ref()
+const newProject = ref({
+    name: '',
+    decription: ''
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
