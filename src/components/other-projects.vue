5234cb86577e8f961b9cd155d76ab788c5bc8802
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-17 21:02:34 +0800

    feat: add other project list component

---
+++ src/components/other-projects.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <Card class="mx-8">
+            <template #title>
+                <span class="text-h3">
+                    Other Projects
+                </span>
+            </template>
+            <template #content>
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <div v-for="project in otherProjects" :key="project.id">
+                        <OtherProjectCard :project="project">
+                        </OtherProjectCard>
+                    </div>
+                </div>
+            </template>
+        </Card>
+    </div>
+</template>
+
+<script setup>
+const { retrieveProjects, getOtherProjects } = useCurrentProjects()
+const otherProjects = useOtherProjects()
+
+await retrieveProjects()
+await getOtherProjects()
+
+</script>
+
+<style lang="scss" scoped></style>
