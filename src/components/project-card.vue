079832de12123ab285dbfc91d6257ca3cf19ed61
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-17 21:00:51 +0800

    ui: card color and click handler

--- src/components/project-card.vue
+++ src/components/project-card.vue
@@ -1,17 +1,23 @@
 <template>
-    <Card class="bg-gradient-to-tr from-blue-500 to-cyan-300" @click="navigateTo(`/project/${project.id}`)">
+    <Card class="bg-gradient-to-tr from-blue-500 to-cyan-300">
         <template #title>
-            <div class="text-h5 text-on-primary">
+            <div class="text-h4 text-on-primary font-bold hover:cursor-pointer" @click="navigateTo(`/project/${project.id}`)">
                 {{ project.name }}
             </div>
         </template>
+        
     </Card>
 </template>
 
 <script setup>
-const { project }  = defineProps(['project'])
+const { project } = defineProps(['project'])
+
+
+
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+.p-card .p-card-content {
+    padding: 0;
+}
 </style>
