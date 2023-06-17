84e9746f8c84be097096e7d18a312b228570ed44
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-17 19:28:33 +0800

    ui: colors and layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="min-h-[100dvh] h-full bg-surface-ground">
+    <div class="min-h-[100dvh] h-full bg-gradient-to-br from-teal-100 to-green-100">
         <div class="border-b bg-surface-card">
             <Topbar class="container mx-auto" />
         </div>
         <main>
             <div class="container mx-auto grid grid-cols-12">
-                <div class="col-span-12">
+                <div class="col-span-12 md:col-span-8 md:col-start-3">
                     <slot />
-                </div>
+                </div>                
             </div>
+            
         </main>
     </div>
     <div>
