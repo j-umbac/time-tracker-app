aa14c23b16168310c97310f76cf29a0774b251ae
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-17 21:07:30 +0800

    feat: add sign out button

--- src/components/topbar.vue
+++ src/components/topbar.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
-        <h2>
+        <h2 class="text-h2 font-bold sm:block">
             <NuxtLink to="/">
                 Keepr
             </NuxtLink>
         </h2>
+        <div>
+            <Button icon="pi pi-sign-out" severity="info" text rounded raised />
+        </div>
     </div>
 </template>
 
