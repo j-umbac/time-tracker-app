b847367bc066a17fa2befc70830495148074a0ca
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-17 20:55:17 +0800

    feat: add project item handlers

--- src/composables/use-project.js
+++ src/composables/use-project.js
@@ -2,6 +2,8 @@ export function useProject() {
     const supabase = useSupabaseClient()
     const errorMsg = ref()
     const project = ref()
+    const entries = ref()
+    const workHours = ref(0)
 
     async function retrieveProject(id) {
         const { data, error } = await supabase
@@ -18,9 +20,86 @@ export function useProject() {
         }
     }
 
+    async function retrieveEntries(id) {
+        const { data, error } = await supabase
+            .from("project_entry")
+            .select("*, users(name)")
+            .eq("project_id", id)
+        if (error) {
+            console.log(error)
+            errorMsg.value = error
+        } else {
+            entries.value = data
+            errorMsg.value = null
+        }
+    }
+
+    async function retrieveWeekEntries(id) {
+        const dateTo = new Date()
+        const dateFrom = new Date()
+        dateFrom.setDate(dateTo.getDate()-5)
+
+        const { data, error } = await supabase
+            .from("project_entry")
+            .select("*, users(name)")
+            .lt('date', dateTo.toISOString())
+            .gt('date', dateFrom.toISOString())
+            .eq("project_id", id)
+        if (error) {
+            console.log(error)
+            errorMsg.value = error
+        } else {
+            entries.value = data
+            errorMsg.value = null
+        }
+    }
+
+    async function getHours() {
+        workHours.value=0
+        if (entries.value.length) {
+            for (let i = 0; i < entries.value.length; i++) {
+                workHours.value = workHours.value + entries.value[i].hours_worked
+            }
+        }
+    }
+
+    function convertDuration(duration) {
+        var days = Math.floor(duration / (24 * 60 * 60));
+        var hours = Math.floor((duration % (24 * 60 * 60)) / (60 * 60));
+        var minutes = Math.floor((duration % (60 * 60)) / 60);
+        var seconds = Math.floor(duration % 60);
+
+        return {
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds
+        };
+    }
+
+    function formatDateTime(dateTime) {
+        let date = new Date(dateTime)
+        return date.toLocaleString('en-US', {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric"
+        })
+    }
+
     return {
         errorMsg,
         retrieveProject,
-        project
+        retrieveEntries,
+        getHours,
+        project,
+        entries,
+        workHours,
+        convertDuration,
+        formatDateTime,
+        retrieveWeekEntries
     }
 }
