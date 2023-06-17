e9cf134bbe8e1476a92d7393b98dd9f5a0b18fa7
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-17 20:12:37 +0800

    infra: url config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const getUrl = () => {
+  let siteUrl =
+    process?.env?.NUXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NUXT_ENV_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/';
+  // Make sure to include `https://` when not localhost.
+  siteUrl = siteUrl.includes('http') ? siteUrl : `https://${siteUrl}`;
+  // Make sure to including trailing `/`.
+  siteUrl = siteUrl.charAt(siteUrl.length - 1) === '/' ? siteUrl : `${siteUrl}/`;
+  return siteUrl;
+};
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   srcDir: './src',
