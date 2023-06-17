5ae79fd5815545e34817628ce6577faee20ece3f
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-17 21:21:01 +0800

    ui: topbar layout

--- src/components/topbar.vue
+++ src/components/topbar.vue
@@ -1,11 +1,11 @@
 <template>
-    <div>
+    <div class="flex h-[70px] justify-between py-8 items-center mx-4 sm:mx-auto">
         <h2 class="text-h2 font-bold sm:block">
             <NuxtLink to="/">
                 Keepr
             </NuxtLink>
         </h2>
-        <div>
+        <div class="mr-8 md:mr-0">
             <Button icon="pi pi-sign-out" severity="info" text rounded raised />
         </div>
     </div>
