642e8d1cb8b9785b457d364110e7c9fa743b48a0
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-17 22:19:46 +0800

    feat: add confirm dialog component

--- src/plugins/primevue.js
+++ src/plugins/primevue.js
@@ -8,21 +8,27 @@ import Divider from 'primevue/divider'
 import Textarea from 'primevue/textarea'
 import InputNumber from 'primevue/inputnumber'
 import Tag from 'primevue/tag'
-import ToggleButton from 'primevue/togglebutton';
-import Tooltip from 'primevue/tooltip';
+import ToggleButton from 'primevue/togglebutton'
+import Tooltip from 'primevue/tooltip'
+import ConfirmDialog from 'primevue/confirmdialog'
+import ConfirmationService from 'primevue/confirmationservice'
+
 
 //Auto-import Primevue components to project
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, { ripple: true })
+    nuxtApp.vueApp.use(ConfirmationService)
     nuxtApp.vueApp.component('Card', Card)
-    nuxtApp.vueApp.component('Button', Button);
-    nuxtApp.vueApp.component('InputText', InputText);
-    nuxtApp.vueApp.component('Password', Password);
-    nuxtApp.vueApp.component('Timeline', Timeline);
-    nuxtApp.vueApp.component('Divider', Divider);
-    nuxtApp.vueApp.component('Textarea', Textarea);
-    nuxtApp.vueApp.component('InputNumber', InputNumber);
-    nuxtApp.vueApp.component('Tag', Tag);
-    nuxtApp.vueApp.component('ToggleButton', ToggleButton);
-    nuxtApp.vueApp.directive('tooltip', Tooltip);
+    nuxtApp.vueApp.component('Button', Button)
+    nuxtApp.vueApp.component('InputText', InputText)
+    nuxtApp.vueApp.component('Password', Password)
+    nuxtApp.vueApp.component('Timeline', Timeline)
+    nuxtApp.vueApp.component('Divider', Divider)
+    nuxtApp.vueApp.component('Textarea', Textarea)
+    nuxtApp.vueApp.component('InputNumber', InputNumber)
+    nuxtApp.vueApp.component('Tag', Tag)
+    nuxtApp.vueApp.component('ToggleButton', ToggleButton)
+    nuxtApp.vueApp.directive('tooltip', Tooltip)
+    nuxtApp.vueApp.component('ConfirmDialog', ConfirmDialog)
+
 });
