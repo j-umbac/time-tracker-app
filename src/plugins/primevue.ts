ff3f87211b8f6c257f4b2bd51c45a11ab9b18003
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-17 19:15:38 +0800

    feat: add timeline component

--- src/plugins/primevue.ts
+++ src/plugins/primevue.ts
@@ -3,13 +3,15 @@ import Card from 'primevue/card'
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
 import Password from 'primevue/password'
+import Timeline from 'primevue/timeline'
 
-//Autoimport Primevue components to project
+//Auto-import Primevue components to project
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, { ripple: true })
     nuxtApp.vueApp.component('Card', Card)
     nuxtApp.vueApp.component('Button', Button);
     nuxtApp.vueApp.component('InputText', InputText);
     nuxtApp.vueApp.component('Password', Password);
+    nuxtApp.vueApp.component('Timeline', Timeline);
 
 });
