b49ab430303b15d07e481c82e73f0f338924d617
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-17 19:21:02 +0800

    feat: add new task component

---
+++ src/components/add-task.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="space-y-8 my-8">
+        <Card class="mx-8">
+            <template #title>
+                <div class="flex space-x-2 items-center">
+                    <Button icon="pi pi-arrow-left" raised rounded text @click="navigateTo(`/project/${project.id}`)" />
+                    <span class="text-h">Record New Task</span>
+                </div>
+            </template>
+            <template #subtitle>
+                {{ project.name }}
+            </template>
+           
+        </Card>
+    </div>
+</template>
+
+<script setup>
+const id = useRoute().params
+const { retrieveProject, project } = useProject()
+const { retrieveUser } = useUser()
+await retrieveUser()
+await retrieveProject(id.id)
+
+
+
+
+</script>
+
+<style lang="scss" scoped></style>
