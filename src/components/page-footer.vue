95546ec3bb62bcd9fbdff80fe1b6d0bf4b1647da
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-17 22:48:55 +0800

    fix: footer

--- src/components/page-footer.vue
+++ src/components/page-footer.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="h-36 flex flex-col items-center justify-center space-y-1 font-semibold">
+    <div class="h-36 flex flex-col items-center justify-center space-y-1 font-semibold mx-8">
         <p class="text-on-surface text-[12px]">
             Created by: Joshua Rhey Umbac
         </p>
-        <p class="text-on-surface text-[12px]">
-            for Countable Web Productions | Junior Developer's Techincal Exam
+        <p class="text-on-surface text-[12px] text-center">
+            for Countable Web Productions | Fronte End Devloper Techincal Exam
         </p>
         <p class="text-on-surface text-[12px]">Source: <a class=" underline" href="https://github.com/j-umbac/time-tracker-app">Keepr: Projects Tracker</a></p>
     </div>
