e51831d2a5522ab74a8225947e8233894896ac9d
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-06-17 21:01:55 +0800

    feat: add total hours

--- src/components/project-card.vue
+++ src/components/project-card.vue
@@ -5,7 +5,32 @@
                 {{ project.name }}
             </div>
         </template>
-        
+        <template #content>
+            <div class="flex flex-col">
+                <span class="text-on-primary font-semibold text-h5 hover:cursor-pointer" @click="navigateTo(`/project/${project.id}`)">
+                    Total:
+                    <span v-if="formatDuration.days">
+                        {{ formatDuration.days }}d
+                    </span>
+                    <span v-if="formatDuration.hours">
+                        {{ formatDuration.hours }}h
+                    </span>
+                    <span v-if="formatDuration.minutes">
+                        {{ formatDuration.minutes }}m
+                    </span>
+                    <span v-if="formatDuration.seconds">
+                        {{ formatDuration.seconds }}s
+                    </span>
+                    <span v-else>
+                        <small>No logged hours yet</small>
+                    </span>
+                </span>
+                <div class="flex mt-4 justify-center">
+                    <Button class="bg-surface-a text-on-surface" label="Add Task" icon="pi pi-plus" icon-pos="right"
+                        size="small" rounded raised text plain @click="navigateTo(`/add/task/${project.id}`)" />
+                </div>
+            </div>
+        </template>
     </Card>
 </template>
 
